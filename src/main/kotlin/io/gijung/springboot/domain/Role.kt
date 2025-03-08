fe01b7b0c5552329582bbb2e25f6a2f14ef3a236
GiJungPark <weeds2577@gmail.com>
2025-03-08 13:37:12 +0900

    feat: 사용자 도메인 작성
    - 사용자의 권한을 관리할 Enum과 사용자 계정을 관리할 VO를 분리하여 작성

---
+++ src/main/kotlin/io/gijung/springboot/domain/Role.kt
@@ -0,0 +1,5 @@
+package io.gijung.springboot.domain
+
+enum class Role {
+    MEMBER, ADMIN
+}
---
+++ src/main/kotlin/io/gijung/springboot/domain/UserAccount.kt
@@ -0,0 +1,6 @@
+package io.gijung.springboot.domain
+
+class UserAccount (
+    val email: String,
+    val password: String,
+)
