adeae6339e3028d0bcd34410ea2aafae0edae68c
GiJungPark <weeds2577@gmail.com>
2025-06-29 15:05:46 +0900

    init project

---
+++ src/main/kotlin/io/gijung/aichatbot/AiChatBotApplication.kt
@@ -0,0 +1,11 @@
+package io.gijung.aichatbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiChatBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<AiChatBotApplication>(*args)
+}
---
+++ src/test/kotlin/io/gijung/aichatbot/AiChatBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.gijung.aichatbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiChatBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
