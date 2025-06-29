262d04e1112f23c92b3878ffab0ef48cbdf81dc1
GiJungPark <weeds2577@gmail.com>
2025-06-29 21:58:34 +0900

    [FEAT] user_profile, user_account 테이블명 지정 및 이메일 인덱스 추가
    - UserProfileEntity, UserAccountEntity에 @Table(name=...) 명시
    - user_account 테이블의 email 컬럼에 인덱스 추가로 조회 성능 개선

--- src/main/kotlin/io/gijung/aichatbot/domain/user/UserAccountEntity.kt
+++ src/main/kotlin/io/gijung/aichatbot/domain/user/UserAccountEntity.kt
@@ -3,8 +3,14 @@ package io.gijung.aichatbot.domain.user
 import io.gijung.aichatbot.repository.entity.BaseEntity
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
 
 @Entity
+@Table(
+    name = "user_account",
+    indexes = [Index(name = "idx_user_account_email", columnList = "email")]
+)
 data class UserAccountEntity(
     @Id
     val id: String,
--- src/main/kotlin/io/gijung/aichatbot/domain/user/UserProfileEntity.kt
+++ src/main/kotlin/io/gijung/aichatbot/domain/user/UserProfileEntity.kt
@@ -3,8 +3,10 @@ package io.gijung.aichatbot.domain.user
 import io.gijung.aichatbot.repository.entity.BaseEntity
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "user_profile")
 data class UserProfileEntity(
     @Id
     val id: String,
