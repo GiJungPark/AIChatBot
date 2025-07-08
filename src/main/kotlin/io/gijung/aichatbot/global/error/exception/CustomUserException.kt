9157dba3d00f4f871815a1df17ab86adb8532c5d
GiJungPark <weeds2577@gmail.com>
2025-07-08 15:50:31 +0900

    [REMOVE] 사용되지 않는 사용자 예외 코드 제거

--- src/main/kotlin/io/gijung/aichatbot/global/error/exception/CustomUserException.kt
+++
@@ -1,6 +0,0 @@
-package io.gijung.aichatbot.global.error.exception
-
-import io.gijung.aichatbot.global.error.code.CustomUserError
-
-class CustomUserException(error: CustomUserError, message: String) :
-    AbstractCustomException(error = error, message = message)
