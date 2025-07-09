6f18cfe82a5e575934139d0068aa3f0666caa88b
GiJungPark <weeds2577@gmail.com>
2025-07-09 20:20:34 +0900

    [REFACTOR] user 패키지 인프라 영속성 계층 구현

---
+++ src/main/kotlin/io/gijung/aichatbot/user/infrastructure/persistence/UserProfileRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package io.gijung.aichatbot.user.infrastructure.persistence
+
+import io.gijung.aichatbot.user.application.port.out.UserProfileRepository
+import io.gijung.aichatbot.user.domain.model.UserProfile
+import io.gijung.aichatbot.user.domain.model.vo.UserId
+import org.springframework.stereotype.Repository
+
+@Repository
+class UserProfileRepositoryImpl(
+    private val userProfileJpaRepository: UserProfileJpaRepository,
+    private val userProfileMapper: UserProfileMapper
+) : UserProfileRepository {
+    override fun existsById(id: UserId): Boolean {
+        return userProfileJpaRepository.existsById(id.value)
+    }
+
+    override fun save(userProfile: UserProfile) {
+        val entity = userProfileMapper.toEntity(userProfile)
+
+        userProfileJpaRepository.save(entity)
+    }
+}
